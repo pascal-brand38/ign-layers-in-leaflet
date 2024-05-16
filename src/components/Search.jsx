a6827114c77518c326e8f9c7e1c358ce7bcca5a5
Pascal Brand <pascal.brand38@gmail.com>
2024-05-16 20:46:05 +0200

    search bar
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,9 @@
 // Copyright (c) Pascal Brand
 // MIT License
 
+// https://react-icons.github.io/react-icons/
+import { FaMagnifyingGlass } from "react-icons/fa6";
+
 function Search({searchTerm, setSearchTerm}) {
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
@@ -8,7 +11,10 @@ function Search({searchTerm, setSearchTerm}) {
 
 
   return (
-      <input type="text" value={searchTerm} onChange={handleChange} />
+    <div className="search-bar">
+        <FaMagnifyingGlass className="search-bar__icon" />
+        <input className="search-bar__input" type="text" value={searchTerm} onChange={handleChange} />
+    </div>
   );
 }
 
