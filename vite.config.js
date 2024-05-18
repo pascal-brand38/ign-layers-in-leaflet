ea537c53c5981d07b213dcba1701f9eed566a4f6
Pascal Brand <pascal.brand38@gmail.com>
2024-05-18 19:39:54 +0200

    deploy
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/ign-layers-in-leaflet/',   // https://dev.to/shashannkbawa/deploying-vite-app-to-github-pages-3ane
   plugins: [react()],
 })
